d6cc6a5c3192eb396628f90ce1a6de479fa37244
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-09 00:35:12 +0500

    Algorithm

---
+++ Algorithm-py/lesson1.py
@@ -0,0 +1,47 @@
+# Algorithm
+
+def hisobla():
+    son1 = int(input("1-sonni kiriting: "))
+    son2 = int(input("2-sonni kiriting: "))
+    summa = son1 + son2
+    return summa
+
+
+print(hisobla())
+
+# Katta son topish
+
+
+def eng_katta(a, b, c):
+    print(a, b, c)
+    if a > b:
+        if a > c:
+            javob = f"eng katta son `{a}`."
+        else:
+            javob = f"eng katta son `{c}`."
+    elif a < b:
+        if b > c:
+            javob = f"eng katta son `{b}`."
+        else:
+            javob = f"eng katta son `{c}`."
+
+    return javob
+
+
+print(eng_katta(120, 100, 99))
+
+
+# N faktorial
+
+
+def faktoriallar(N):
+    faktorial = 1
+    i = 1
+    for m in range(1, N + 1):
+        faktorial *= m
+        i += 1
+    return faktorial
+
+
+son = int(input("Son kiriting: "))
+print(faktoriallar(son))
---
+++ app.js
@@ -0,0 +1,8 @@
+function faktorial(num) {
+  let natija = 1;
+  for (let i = 1; i <= num; i++) {
+    natija = natija * i;
+  }
+  return natija;
+}
+console.log(faktorial(6));
