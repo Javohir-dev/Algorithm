c6ae73812301d5a204de4f16e43f72428585bf58
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-07-11 22:43:52 +0500

    update

--- Algorithm-py/lesson1.py
+++ Algorithm-py/lesson1.py
@@ -1,45 +1,45 @@
-# Algorithm
+# # Algorithm
 
-def hisobla():
-    son1 = int(input("1-sonni kiriting: "))
-    son2 = int(input("2-sonni kiriting: "))
-    summa = son1 + son2
-    return summa
-
-
-# print(hisobla())
-
-# Katta son topish
+# def hisobla():
+#     son1 = int(input("1-sonni kiriting: "))
+#     son2 = int(input("2-sonni kiriting: "))
+#     summa = son1 + son2
+#     return summa
 
 
-def eng_katta(a, b, c):
-    print(a, b, c)
-    if a > b:
-        if a > c:
-            javob = f"eng katta son `{a}`."
-        else:
-            javob = f"eng katta son `{c}`."
-    elif a < b:
-        if b > c:
-            javob = f"eng katta son `{b}`."
-        else:
-            javob = f"eng katta son `{c}`."
+# # print(hisobla())
 
-    return javob
+# # Katta son topish
 
 
-# print(eng_katta(120, 100, 99))
+# def eng_katta(a, b, c):
+#     print(a, b, c)
+#     if a > b:
+#         if a > c:
+#             javob = f"eng katta son `{a}`."
+#         else:
+#             javob = f"eng katta son `{c}`."
+#     elif a < b:
+#         if b > c:
+#             javob = f"eng katta son `{b}`."
+#         else:
+#             javob = f"eng katta son `{c}`."
+
+#     return javob
 
 
-# N faktorial
+# # print(eng_katta(120, 100, 99))
+
+
+# # N faktorial
 
 
 def faktoriallar(N):
     faktorial = 1
-    for m in range(1, N + 1):
-        faktorial *= m
+    for i in range(1, N + 1):
+        faktorial *= i
     return faktorial
 
 
 son = int(input("Son kiriting: "))
-# print(faktoriallar(son))
+print(faktoriallar(son))
--- Algorithm-py/searching.py
+++ Algorithm-py/searching.py
@@ -1,18 +1,27 @@
-def binary_search(list, item):
-    low = 0
-    high = len(list)-1
-    while low <= high:
-        mid = (low + high)//2
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+# def binary_search(list, item):
+#     low = 0
+#     high = len(list)-1
+#     while low <= high:
+#         mid = (low + high)//2
+#         guess = list[mid]
+#         if guess == item:
+#             return mid
+#         if guess > item:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return None
 
 
-myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-          11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-print(binary_search(myList, 20))
+# myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+#           11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# print(binary_search(myList, 20))
+
+def findNum(N=100):
+    for n in range(1, 100):
+        ans = input(f"Siz {n}-o'yladingiz? (y/n)")
+        if ans == 'y':
+            print("Yutdim!")
+            return n
+
+findNum(100)
